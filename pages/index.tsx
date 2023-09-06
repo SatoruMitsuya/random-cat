87054a1c264222c4a303277064c7de2c2ab11fa2
satoru rcat <you@example.com>
2023-09-07 08:11:10 +0900

    One more cat!

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,7 +35,9 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
 
   return (
     <div className={styles.page}>
-      <button onClick={handleClick} className={styles.button}>ほかの猫も見る</button>
+      <button onClick={handleClick} className={styles.button}>
+        One more cat!
+      </button>
       <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} /> }</div>
     </div>
   );
